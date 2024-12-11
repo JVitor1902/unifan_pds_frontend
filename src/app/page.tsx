a1a8c0c850f64f395f6d378194ccc55dddce57cc
JVitor1902 <tititojv47@gmail.com>
2024-12-11 12:36:59 -0300

    01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,78 +3,66 @@ import { useEffect, useState, useRef, FormEvent } from "react";
 import { FiTrash, FiEdit, FiCheck } from "react-icons/fi"
 import { api } from "./api";
 
-interface PerfilProps {
+interface UsuarioProps {
   id: string;
-  descricao: string;
-  criar_usuario: boolean;
-  editar_usuario: boolean;
-  excluir_usuario: boolean;
-  ler_usuario: boolean;
+  matricula: string;
+  perfil_id: string;
 }
 
-export default function Perfil() {
+export default function Usuario() {
 
   // Linkar os inputs
-  const descricaoRef = useRef<HTMLInputElement | null>(null)
-  const criar_usuarioRef = useRef<HTMLInputElement | null>(null)
-  const editar_usuarioRef = useRef<HTMLInputElement | null>(null)
-  const excluir_usuarioRef = useRef<HTMLInputElement | null>(null)
-  const ler_usuarioRef = useRef<HTMLInputElement | null>(null)
-
+  const matriculaRef = useRef<HTMLInputElement | null>(null)
   // Inicializa lista de tarefas da página como lista vazia
-  const [perfis, setPerfil] = useState<PerfilProps[]>([])
+  const [usuarios, setUsuario] = useState<UsuarioProps[]>([])
 
   // Ao renderizar a página, chama a função "readTasks"
   useEffect(() => {
-    readPerfil();
+    readUsuario();
   }, [])
 
   // Busca as tarefas no banco de dados via API
-  async function readPerfil() {
+  async function readUsuario() {
     try{
-    const response = await api.get("/perfis")
+    const response = await api.get("/usuarios")
     console.log(response.data)
-    setPerfil(Array.isArray(response.data) ? response.data : [])
+    setUsuario(Array.isArray(response.data) ? response.data : [])
     }
     catch(error){
       console.error("Error fetching tasks:", error);
-      setPerfil([])
+      setUsuario([])
 
     }
   }
 
   // Cria uma nova tarefa
-  async function createPerfil(event: FormEvent) {
+  async function createUsuario(event: FormEvent) {
     event.preventDefault()
-    const response = await api.post("/perfis", {
-      descricao: descricaoRef.current?.value,
-      criar_usuario: criar_usuarioRef,
-      editar_usuario: editar_usuarioRef,
-      excluir_usuario: excluir_usuarioRef,
-      ler_usuario: ler_usuarioRef
+    const response = await api.post("/usuarios", {
+      descricao: matriculaRef.current?.value,
     }) 
-    setPerfil(allPerfil => [...allPerfil, response.data])
+    setUsuario(allUsuario => [...allUsuario, response.data])
   }
 
   // Deleta uma tarefa
-  async function deletePerfil(id: string){
+  async function deleteUsuario(id: string){
     try{
-      await api.delete("/perfis/" + id)
-      const allPerfil = perfis.filter((perfil) => perfil.id !== id)
-      setPerfil(allPerfil)
+      await api.delete("/usuarios/" + id)
+      const allUsuario= usuarios.filter((usuario) => usuario.id !== id)
+      setUsuario(allUsuario)
     }
     catch(err){
       alert(err)
     }
   }
 
-  async function setPerfilDone(id:string) {
+  async function setUsuarioDone(id:string) {
     try {
-      await api.put("/perfis/" + id, {
+      await api.put("/usuarios/" + id, {
         status: true,
       })
-      const response = await api.get("/perfis")
-      setPerfil(response.data)
+      const response = await api.get("/usuarios")
+      setUsuario(response.data)
     }
     catch(err){
       alert(err)
@@ -85,35 +73,31 @@ export default function Perfil() {
     <div className="w-full min-h-screen bg-slate-500 flex justify-center px-4">
       <main className="my-10 w-full lg:max-w-5xl">
         <section>
-          <h1 className="text-4xl text-slate-200 font-medium text-center">Perfis</h1>
+          <h1 className="text-4xl text-slate-200 font-medium text-center">Usuários</h1> 
 
-          <form className="flex flex-col my-6" onSubmit={createPerfil}>
+          <form className="flex flex-col my-6" onSubmit={createUsuario}>
           
-            <label className="text-slate-200">Descrição do Perfil</label>
-            <input type="text" className="w-full mb-5 p-2 rounded" ref={descricaoRef}/>
+            <label className="text-slate-200">Matricula do Usuário</label>
+            <input type="text" className="w-full mb-5 p-2 rounded" ref={matriculaRef}/>
             
-            <input type="submit" value={"Adicionar Perfil"} className="cursor-pointer w-full bg-slate-800 rounded font-medium text-slate-200 p-4" /> 
+            <input type="submit" value={"Adicionar Usuário"} className="cursor-pointer w-full bg-slate-800 rounded font-medium text-slate-200 p-4" /> 
           </form>
 
         </section>
         <section className="mt-5 flex flex-col">
 
-          {perfis.length > 0 ? (
-          perfis.map((perfil) => (
-            <article className="w-full bg-slate-200 text-slate-800 p-2 mb-4 rounded relative hover:bg-sky-300" key={perfil.id}>
-              <p>{perfil.descricao}</p>
-              <p>{perfil.criar_usuario.toString()}</p>
-              <p>{perfil.editar_usuario.toString()}</p>
-              <p>{perfil.excluir_usuario.toString()}</p>
-              <p>{perfil.ler_usuario.toString()}</p>
+          {usuarios.length > 0 ? (
+          usuarios.map((usuario) => (
+            <article className="w-full bg-slate-200 text-slate-800 p-2 mb-4 rounded relative hover:bg-sky-300" key={usuario.id}>
+              <p>{usuario.matricula}</p>
+              
+              <button className="flex absolute right-10 -top-2 bg-green-600 w-7 h-7 items-center justify-center text-slate-200" onClick={() => setUsuarioDone(usuario.id)}><FiCheck></FiCheck></button>
 
-              <button className="flex absolute right-10 -top-2 bg-green-600 w-7 h-7 items-center justify-center text-slate-200" onClick={() => setPerfilDone(perfil.id)}><FiCheck></FiCheck></button>
-
-              <button className="flex absolute right-0 -top-2 bg-red-600 w-7 h-7 items-center justify-center text-slate-200" onClick={() => deletePerfil(perfil.id)}><FiTrash></FiTrash></button>
+              <button className="flex absolute right-0 -top-2 bg-red-600 w-7 h-7 items-center justify-center text-slate-200" onClick={() => deleteUsuario(usuario.id)}><FiTrash></FiTrash></button>
             </article>
           ))
         ) : (
-             <p className="text-center text-gray-500">No perfis available</p>
+             <p className="text-center text-gray-500">No usuarios available</p>
         )}
         </section>
       </main>
